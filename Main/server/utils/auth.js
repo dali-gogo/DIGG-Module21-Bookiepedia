e7560c8b05f04ef4b84af9d2d12a88c6479e235e
Dalia Gomez <daivogg@gmail.com>
2024-08-19 23:22:17 -0600

    Created the schemas folder, including the index.js, resolvers.js and typeDefs.js files. Modified the server.js, auth.js and package.json files. Deleted the user-controller.js file

--- Main/server/utils/auth.js
+++ Main/server/utils/auth.js
@@ -1,16 +1,18 @@
 const jwt = require('jsonwebtoken');
-
-// set token secret and expiration date
+const { GraphQLError } = require('graphql');
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
